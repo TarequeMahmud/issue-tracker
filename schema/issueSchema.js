1eb44d0a6aac9af3945867ed0422152cf915bddc
TarequeMahmud <107052189+TarequeMahmud@users.noreply.github.com>
2024-10-13 22:29:39 +0600

    feat:adding timestamp on issue schema

--- schema/issueSchema.js
+++ schema/issueSchema.js
@@ -1,35 +1,38 @@
 const { default: mongoose } = require("mongoose");
 
-const issueSchema = new mongoose.Schema({
-  project: {
-    type: String,
-    required: true,
+const issueSchema = new mongoose.Schema(
+  {
+    project: {
+      type: String,
+      required: true,
+    },
+    issue_title: {
+      type: String,
+      required: true,
+    },
+    issue_text: {
+      type: String,
+      required: true,
+    },
+    created_by: {
+      type: String,
+      required: true,
+    },
+    assigned_to: {
+      type: String,
+      required: false,
+    },
+    status_text: {
+      type: String,
+      required: false,
+    },
+    open: {
+      type: Boolean,
+      default: true,
+    },
   },
-  issue_title: {
-    type: String,
-    required: true,
-  },
-  issue_text: {
-    type: String,
-    required: true,
-  },
-  created_by: {
-    type: String,
-    required: true,
-  },
-  assigned_to: {
-    type: String,
-    required: false,
-  },
-  status_text: {
-    type: String,
-    required: false,
-  },
-  open: {
-    type: Boolean,
-    default: true,
-  },
-});
+  { timestamps: { createdAt: "created_on", updatedAt: "updated_on" } }
+);
 
-const Url = mongoose.model("Issue", issueSchema);
-module.exports = Url;
+const Issue = mongoose.model("Issue", issueSchema);
+module.exports = Issue;
