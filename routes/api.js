f2864f847baaa0d415b07850d316c3334e4543e1
TarequeMahmud <107052189+TarequeMahmud@users.noreply.github.com>
2024-10-13 22:31:16 +0600

    feat:post issues on databases

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const Issue = require("../schema/issueSchema");
+
 module.exports = function (app) {
   app
     .route("/api/issues/:project")
@@ -8,8 +10,48 @@ module.exports = function (app) {
       let project = req.params.project;
     })
 
-    .post(function (req, res) {
+    .post(async function (req, res) {
       let project = req.params.project;
+      const {
+        issue_title: issueTitle,
+        issue_text: issueText,
+        created_by: createdBy,
+        assigned_to: assignedTo,
+        status_text: statusText,
+      } = req.body;
+      if (!issueTitle || !issueText || !createdBy) {
+        return res.json({ error: "required field(s) missing" });
+      }
+      const issue = {
+        project: project,
+        issue_title: issueTitle,
+        issue_text: issueText,
+        created_by: createdBy,
+        assigned_to: assignedTo,
+        status_text: statusText,
+      };
+      console.log(issue);
+
+      try {
+        const newIssue = Issue(issue);
+        const saveIssue = await newIssue.save();
+
+        return res.json({
+          project: saveIssue.project,
+          issue_title: saveIssue.issue_title,
+          issue_text: saveIssue.issue_text,
+          created_by: saveIssue.created_by,
+          assigned_to: saveIssue.assigned_to || "",
+          status_text: saveIssue.status_text || "",
+          created_on: saveIssue.created_on,
+          updated_on: saveIssue.updated_on,
+          open: saveIssue.open,
+          _id: saveIssue._id,
+        });
+      } catch (error) {
+        console.log(error);
+        res.json(error);
+      }
     })
 
     .put(function (req, res) {
