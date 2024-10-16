7f8c9dad8dee2ac9cbeaab459facdfde53740ae1
TarequeMahmud <107052189+TarequeMahmud@users.noreply.github.com>
2024-10-16 10:22:21 +0600

    test(issue): add functional test for POST issues creation route.

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -56,4 +56,110 @@ suite("Functional Tests", function () {
         });
     });
   });
+
+  suite("Test Post Route", () => {
+    //post-1
+    test("Create an issue with every field", (done) => {
+      chai
+        .request(server)
+        .keepOpen()
+        .post("/api/issues/apitest")
+        .send({
+          issue_title: "New Issue",
+          issue_text: "test",
+          created_by: "Tester",
+          assigned_to: "This is a test issue",
+          status_text: "In Progress",
+        })
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+
+          assert.isObject(res.body, "response should be an object");
+          assert.property(
+            res.body,
+            "issue_title",
+            "Issue object should have a title"
+          );
+          assert.property(
+            res.body,
+            "issue_text",
+            "Issue object should have issue text"
+          );
+          assert.property(
+            res.body,
+            "created_by",
+            "Issue object should have created by"
+          );
+          assert.property(
+            res.body,
+            "assigned_to",
+            "Issue object should have assigned to"
+          );
+          assert.property(
+            res.body,
+            "status_text",
+            "Issue object should have status text"
+          );
+          done();
+        });
+    });
+    //post-2
+    test("Create an issue with only required fields", (done) => {
+      chai
+        .request(server)
+        .keepOpen()
+        .post("/api/issues/apitest")
+        .send({
+          issue_title: "New Issue",
+          issue_text: "test",
+          created_by: "Tester",
+        })
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+
+          assert.isObject(res.body, "response should be an object");
+          assert.property(
+            res.body,
+            "issue_title",
+            "Issue object should have a title"
+          );
+          assert.property(
+            res.body,
+            "issue_text",
+            "Issue object should have issue text"
+          );
+          assert.property(
+            res.body,
+            "created_by",
+            "Issue object should have created by"
+          );
+
+          done();
+        });
+    });
+    //post-3
+    test("Create an issue with missing required fields", (done) => {
+      chai
+        .request(server)
+        .keepOpen()
+        .post("/api/issues/apitest")
+        .send({
+          issue_title: "New Issue",
+          issue_text: "test",
+        })
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+
+          assert.isObject(res.body, "response should be an object");
+          assert.property(
+            res.body,
+            "error",
+            "response should have an error property"
+          );
+          assert.equal(res.body.error, "required field(s) missing");
+
+          done();
+        });
+    });
+  });
 });
